5f4917942f0517b8018cdac30d7d01675c9f4c23
Francesco <francesco.bellocci@stud.unifi.it>
2017-07-18 09:40:33 +0200

    Modified class mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtGui>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    GregorianCalendar();
+
 }
+
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "gregoriancalendar.h"
+#include <QtGui>
+#include <qlcdnumber.h>
+
+//class QLCDNumber;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    //void MoreYear();
+    //void LessYear();
+    //void MoreMonth();
+    //void LessMonth();
+
+
 private:
     Ui::MainWindow *ui;
+    GregorianCalendar gc;
+    QLCDNumber *lcdyear;
 };
 
 #endif // MAINWINDOW_H
